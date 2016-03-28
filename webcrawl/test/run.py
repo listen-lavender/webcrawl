257899dff746921e5be2aa3bbbaef095d9f13a55
LavenderUni <uni@LavenderUnideiMac.local>
2016-03-28 11:58:20 +0800

    add Test and Deploy tool travis ci

---
+++ webcrawl/test/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from __future__ import absolute_import, division, print_function, with_statement
+
+
+from turbo.test.util import unittest
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(main())
